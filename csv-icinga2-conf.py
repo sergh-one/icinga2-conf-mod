ebd648a53549cfcbe08e6f0e01fa12fbc83a0eab
sergh-one <34002746+sergh-one@users.noreply.github.com>
2022-10-11 10:06:35 +0300

    Add files via upload
    add arguments

--- csv-icinga2-conf.py
+++ csv-icinga2-conf.py
@@ -3,14 +3,17 @@
 
 import csv
 import re
+import argparse
 
 # читаем csv и записываем список словарей для последующей сверки
-list_dicts = []
-with open('checks.csv') as f:
-    reader = csv.DictReader(f)
-    for dict in reader:
-        if re.search('FALSE|ЛОЖЬ', dict['Перенесено?']) and dict['Хост'] != '':
-            list_dicts.append(dict)
+def get_from_csv(full_path):
+    list_dicts = []
+    with open(full_path) as f:
+        reader = csv.DictReader(f)
+        for dict in reader:
+            if re.search('FALSE|ЛОЖЬ', dict['Перенесено?']) and dict['Хост'] != '':
+                list_dicts.append(dict)
+    return list_dicts
 
 # читаем основной файл конфигурации с которого нужно перенести хосты, проходимся по нему 
 # регулярными выражениями со значениями из словарей выше, после результат пишем в файл.
@@ -44,6 +47,8 @@ with open('manager.conf', 'r+') as conf:
                 list_objects_with_changed_host.append(ic2_mod_object)
                 continue
     
+
+
     
 # Проверяем каждый объект - хост назначения также становится файлом конфига, куда требуется записать объекты.
 def object_to_file(ic2_object):
@@ -57,6 +62,17 @@ def object_to_file(ic2_object):
             print(match, ": already present in file")
         else:
             config.write(ic2_object + '\n\n')
+
+
+def set_args():
+    parser = argparse.ArgumentParser(description='Transfer icinga2 checks from one config to others by get destination form csv file')
+    parser.add_argument('--csv', type=str, required=True, help='Path to csv icinga2 checks transfer file')
+    parser.add_argument('--from', type=str, required=True, help='The name or full path of config which from we a transfer icinga2 checks')
+    arguments = parser.parse_args()
+    return(arguments)
             
 for ic2_object in list_objects_with_changed_host:
     object_to_file(ic2_object)
+
+args = set_args()
+CSV_DICTS = get_from_csv(args.csv)
